815516f44b73a61b973af748fc613eecaaa1c463
Nicholas Rios <rios.nicholas@protonmail.com>
2024-03-23 17:06:14 -0400

    Delete and reinitialize project

--- andean-coffeehouse/src/main.jsx
+++ andean-coffeehouse/src/main.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from './App.jsx'
 import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
-
-<BrowserRouter>
-  <Routes>
-      <Route index={true} path="/" element={<App />} />
-  </Routes>
-</BrowserRouter>
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
