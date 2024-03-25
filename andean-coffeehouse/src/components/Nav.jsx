ab76aa244cf9fb2991f34bc489729ee7cdabbb44
Nicholas Rios <rios.nicholas@protonmail.com>
2024-03-25 13:58:25 -0400

    Update background color and margin

--- andean-coffeehouse/src/components/Nav.jsx
+++ andean-coffeehouse/src/components/Nav.jsx
@@ -2,25 +2,25 @@ import { Outlet, Link } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <nav className="flex p-5 bg-yellow-50 text-black justify-between">
+        <nav className="flex p-5 bg-yellow-50 text-black justify-between sticky top-0">
             <label className="font-extrabold mx-5">Andean Coffeehouse</label>
             <ul className="list-none flex font-light mx-5">
                 <li className="px-2 hover:underline hover:cursor-pointer">
                     <Link to="/">Home</Link>    
                 </li>
-                <li className="px-2 hover:underline hover:cursor-pointer">
+                <li className="mx-2 hover:underline hover:cursor-pointer">
                     <Link to="/about">Our Story</Link>
                 </li>
-                <li className="px-2 hover:underline hover:cursor-pointer">
+                <li className="mx-2 hover:underline hover:cursor-pointer">
                     <Link to="/menu">Menu</Link>
                 </li>
-                <li className="px-2 hover:underline hover:cursor-pointer">
+                <li className="mx-2 hover:underline hover:cursor-pointer">
                     <Link to="/blog">Blog</Link>
                 </li>
-                <li className="px-2 hover:underline hover:cursor-pointer">
+                <li className="mx-2 hover:underline hover:cursor-pointer">
                     <Link to="/locations">Locations</Link>
                 </li>
-                <li className="px-2 hover:underline hover:cursor-pointer">
+                <li className="mx-2 hover:underline hover:cursor-pointer">
                     <Link to="/contact">Contact Us</Link>
                 </li>
             </ul>
