8e33f88de5d9eb85dc40d91ce61009b465a70c87
Nicholas Rios <rios.nicholas@protonmail.com>
2024-03-24 23:40:50 -0400

    Add missing page to router

--- andean-coffeehouse/src/main.jsx
+++ andean-coffeehouse/src/main.jsx
@@ -6,6 +6,7 @@ import About from './pages/About.jsx'
 import Menu from './pages/Menu.jsx'
 import Locations from './pages/Locations.jsx'
 import Blog from './pages/Blog.jsx'
+import Contact from './pages/Contact.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Route index={true} path="/menu" element={<Menu />} />
       <Route index={true} path="/blog" element={<Locations />} />
       <Route index={true} path="/locations" element={<Blog />} />
+      <Route index={true} path="/contact" element={<Contact />} />
     </Routes>
   </BrowserRouter>
 )
