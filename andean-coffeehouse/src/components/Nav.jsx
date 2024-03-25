c7c45bc9db9e9de3fbf797d93c17490c0d9cf267
Nicholas Rios <rios.nicholas@protonmail.com>
2024-03-24 21:30:08 -0400

    Update CSS and begin routing implementation

--- andean-coffeehouse/src/components/Nav.jsx
+++ andean-coffeehouse/src/components/Nav.jsx
@@ -1,18 +1,30 @@
+import { Outlet, Link } from "react-router-dom";
+
 const Nav = () => {
     return (
-        <div className="flex w-screen p-5 bg-yellow-50 text-black">
-            <nav className="flex justify-between">
-                <label className="font-extrabold mx-5">Andean Coffeehouse</label>
-                <ul className="list-none flex font-light mx-5">
-                    <li className="">Home</li>
-                    <li>Our Story</li>
-                    <li>Menu</li>
-                    <li>Blog</li>
-                    <li>Locations</li>
-                    <li>Contact Us</li>
-                </ul>
-            </nav>
-        </div>
+        <nav className="flex p-5 bg-yellow-50 text-black justify-between">
+            <label className="font-extrabold mx-5">Andean Coffeehouse</label>
+            <ul className="list-none flex font-light mx-5">
+                <li className="px-2 hover:underline hover:cursor-pointer">
+                    <Link to="/">Home</Link>    
+                </li>
+                <li className="px-2 hover:underline hover:cursor-pointer">
+                    <Link to="/about">Our Story</Link>
+                </li>
+                <li className="px-2 hover:underline hover:cursor-pointer">
+                    <Link to="/menu">Menu</Link>
+                </li>
+                <li className="px-2 hover:underline hover:cursor-pointer">
+                    <Link to="/blog">Blog</Link>
+                </li>
+                <li className="px-2 hover:underline hover:cursor-pointer">
+                    <Link to="/locations">Locations</Link>
+                </li>
+                <li className="px-2 hover:underline hover:cursor-pointer">
+                    <Link to="/contact">Contact Us</Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
