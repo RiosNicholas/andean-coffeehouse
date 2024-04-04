64b2527f66d1935d3472d4af17db2f9757e3714e
Nicholas Rios <rios.nicholas@protonmail.com>
2024-04-03 23:26:53 -0400

    Update file type to SQL

---
+++ backend/andean_coffeeshop.sql
@@ -0,0 +1,28 @@
+CREATE TABLE Customer (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(300),
+    customer_phone VARCHAR(15),
+    customer_email VARCHAR(300),
+    is_member BOOLEAN
+);
+
+CREATE TABLE Coffee (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(300),
+    unit_price DECIMAL(10, 2),
+    flavor_description VARCHAR(300),
+    origin VARCHAR(300),
+    drink_type ENUM('Espresso', 'Latte', 'Cappuccino', 'Americano', 'Macchiato', 'Mocha', 'Affogato'),
+    bean_type ENUM('Arabica', 'Robusta'),
+    roast ENUM('Light', 'Medium', 'Dark')
+);
+
+CREATE TABLE Order (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    date DATE,
+    total_price DECIMAL(10, 2),
+    FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
+
+
