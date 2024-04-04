2433da15d65c747274f4e7a0cce7eb9416ff63ff
Nicholas Rios <rios.nicholas@protonmail.com>
2024-04-03 23:08:25 -0400

    Add black overlay for text readability

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,20 +1,21 @@
-import coverImage from '../assets/pexels-jonathan-borba-2878713.jpg'
-
+import coverImage from '../assets/pexels-jonathan-borba-2878713.jpg';
 
 const Hero = () => {
     return (
         <div className="relative">
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full text-center">
-                <h1 className="text-white font-bold text-2xl md:text-4xl uppercase shadow">
+            <div className="absolute top-0 left-0 w-full h-full bg-black opacity-50"></div>
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-64 md:-translate-y-24 w-full text-center">
+                <h1 className="text-white font-bold text-2xl md:text-4xl uppercase">
                     Experience one of a kind coffee
                 </h1>
             </div>
-            <img 
+            <img
                 src={coverImage}
-                className="w-screen object-cover h-dvh max- shadow"
+                className="w-screen h-dvh object-cover"
+                alt="Coffee table setup with pour-over coffee"
             />
         </div>
     );
-}
+};
 
 export default Hero;
