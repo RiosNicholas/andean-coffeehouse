5ac0d9cf470be1efc1249e717ef1d929152e0932
Nicholas Rios <rios.nicholas@protonmail.com>
2024-04-06 18:58:48 -0400

    Create PHP database connection

--- backend/api/index.php
+++ backend/api/index.php
@@ -1,3 +1,16 @@
 <?php
+// Fetch credentials using environment variables
+$db_username = getenv('DB_USERNAME');
+$db_password = getenv('DB_PASSWORD');
+$db_name = getenv('DB_NAME');
+$servername = getenv('SERVER_NAME');
 
+// Create database connection
+$conn = new mysqli($servername, $db_username, $db_password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->close(); 
 ?>
