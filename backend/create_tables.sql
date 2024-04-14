9ce1e047959ed09332ab13797380010ecb0bc80e
Nicholas Rios <rios.nicholas@protonmail.com>
2024-04-13 21:42:05 -0400

    Update SQL queries

--- backend/create_tables.sql
+++ backend/create_tables.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE IF NOT EXISTS andean_coffeeshop_db;
 
 CREATE TABLE Customer (
-    customer_id INT AUTO_INCREMENT,
+    customer_id INT PRIMARY KEY AUTO_INCREMENT,
     customer_name VARCHAR(300),
     customer_phone VARCHAR(15),
     customer_email VARCHAR(300),
-    is_member BOOLEAN
+    is_member TINYINT(1)
 );
 
 CREATE TABLE Coffee (
@@ -22,9 +22,16 @@ CREATE TABLE Coffee (
 CREATE TABLE Order (
     order_id INT PRIMARY KEY,
     customer_id INT,
-    date DATE,
+    date_purchased DATE,
     total_price DECIMAL(10, 2),
     FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
 );
 
+CREATE TABLE Emails (
+    email_id INT PRIMARY KEY AUTO_INCREMENT,
+    contact_name VARCHAR(200),
+    contact_email VARCHAR(200),
+    message TEXT,
+    date_sent DATE
+);
 
