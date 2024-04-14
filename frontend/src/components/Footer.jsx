ad2cb97cc7f076951322816359412b6b3aa55f04
Nicholas Rios <rios.nicholas@protonmail.com>
2024-04-13 22:21:00 -0400

    Update footer placement and text size

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,10 @@
 const Footer = () => {
     return (
-        <footer className="flex justify-center items-center text-center bottom-0">
-            <a className="font-semibold text-center w-full p-8 hover:cursor-pointer hover:underline" href="https://github.com/RiosNicholas/andean-coffeehouse">
+        <footer className="flex justify-center items-center text-center w-full py-4 absolute bottom-0">
+            <a className="font-semibold hover:cursor-pointer hover:underline text-xs" href="https://github.com/RiosNicholas/andean-coffeehouse">
                 Nicholas Rios | 2024
             </a>
         </footer>
-
     );
 }
 
