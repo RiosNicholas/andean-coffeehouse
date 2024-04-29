fe384d22423dbde25dc89018ad6d7ef2329fc4c3
Nicholas Rios <rios.nicholas@protonmail.com>
2024-04-29 15:27:07 -0400

    Update headers for more flexibility

--- andean-coffeehouse/api/get_menu.php
+++ andean-coffeehouse/api/get_menu.php
@@ -1,8 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header('Content-Type: application/json');
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: *");
+// header('Content-Type: application/json');
 
 // Include setup file
 require_once 'index.php';
