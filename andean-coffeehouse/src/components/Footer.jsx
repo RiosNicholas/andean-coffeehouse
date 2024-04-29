a4958deb56ad27b84615fdbee04f5df5c24a1d4e
Nicholas Rios <rios.nicholas@protonmail.com>
2024-04-29 12:51:21 -0400

    Fix issue with overlapping footer

--- andean-coffeehouse/src/components/Footer.jsx
+++ andean-coffeehouse/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="flex justify-center items-center text-center w-full py-4 absolute bottom-0">
+        <footer className="flex justify-center items-center text-center w-full py-4 bottom-0">
             <a className="font-semibold hover:cursor-pointer hover:underline text-xs" href="https://github.com/RiosNicholas/andean-coffeehouse">
                 Nicholas Rios | 2024
             </a>
