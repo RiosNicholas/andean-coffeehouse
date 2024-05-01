97256716443bb0e2187cc8ca1eacf4c5559e4a9e
Nicholas Rios <rios.nicholas@protonmail.com>
2024-05-01 15:08:35 -0400

    Update value in query

--- andean-coffeehouse/queries/insert_customers.sql
+++ andean-coffeehouse/queries/insert_customers.sql
@@ -1,11 +1,11 @@
 INSERT INTO Customer (customer_name, customer_phone, customer_email, is_member) VALUES
     ('Lionel Messi', '111-111-1111', 'lionel.messi@rutgers.edu', 1),
     ('Cristiano Ronaldo', '777-777-7777', 'cristiano.ronaldo@rutgers.edu', 1),
-    ('Neymar Jr.', '888-888-8888', 'neymar.jr@rutgers.edu', 0),
-    ('Robert Lewandowski', '222-222-2222', 'robert.lewandowski@rutgers.edu', 0),
+    ('Neymar Jr.', '888-888-8888', 'neymar.jr@rutgers.edu', 1),
+    ('Robert Lewandowski', '222-222-2222', 'robert.lewandowski@rutgers.edu', 1),
     ('Kylian Mbappe', '333-333-3333', 'kylian.mbappe@rutgers.edu', 1),
-    ('Mo Salah', '444-444-4444', 'mohamed.salah@rutgers.edu', 0),
+    ('Mo Salah', '444-444-4444', 'mohamed.salah@rutgers.edu', 1),
     ('Sergio Ramos', '555-555-5555', 'sergio.ramos@rutgers.edu', 1),
-    ('Kevin De Bruyne', '666-666-6666', 'kevin.debruyne@rutgers.edu', 0),
+    ('Kevin De Bruyne', '666-666-6666', 'kevin.debruyne@rutgers.edu', 1),
     ('Harry Kane', '777-777-7777', 'harry.kane@rutgers.edu', 1),
-    ('Luis Suarez', '888-888-8888', 'luis.suarez@rutgers.edu', 0);
+    ('Luis Suarez', '888-888-8888', 'luis.suarez@rutgers.edu', 1);
