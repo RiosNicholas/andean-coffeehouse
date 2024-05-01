f684a44fc2f0e237774866c3c113e5109728396d
Nicholas Rios <rios.nicholas@protonmail.com>
2024-05-01 14:33:34 -0400

    Create SQL query for inserting locations

---
+++ andean-coffeehouse/queries/create_location_entries.sql
@@ -0,0 +1,6 @@
+INSERT INTO Location (city, state, address, phoneNumber)
+VALUES 
+    ('Elizabeth', 'New Jersey', '123 ABC Street', '(191)232-1232'),
+    ('Newark', 'New Jersey', '123 ABC Street', '(432)123-1232'),
+    ('Jersey City', 'New Jersey', '123 ABC Street', '(934)321-2343'),
+    ('Linden', 'New Jersey', '123 ABC Street', '(234)122-4325');
