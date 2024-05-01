11018a9c094be52e93caabf706eac506b1239d36
Nicholas Rios <rios.nicholas@protonmail.com>
2024-05-01 14:59:29 -0400

    Add missing semicolon

--- andean-coffeehouse/queries/insert_customers.sql
+++ andean-coffeehouse/queries/insert_customers.sql
@@ -4,4 +4,4 @@ INSERT INTO Customer (customer_name, customer_phone, customer_email, is_member) 
     ('Jane Doe', '888-888-8888', 'jane.doe@rutgers.edu', 0),
     ('Emily Johnson', '222-222-2222', 'emily.johnson@rutgers.edu', 0),
     ('Michael Smith', '333-333-3333', 'michael.smith@rutgers.edu', 1),
-    ('Sophia Rodriguez', '444-444-4444', 'sophia.rodriguez@rutgers.edu', 0),
+    ('Sophia Rodriguez', '444-444-4444', 'sophia.rodriguez@rutgers.edu', 0);
