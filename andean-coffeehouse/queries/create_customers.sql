f290eb47780c845097b586103c699394cdb8a83d
Nicholas Rios <rios.nicholas@protonmail.com>
2024-05-01 14:56:20 -0400

    Create SQL query for creating creating customers

---
+++ andean-coffeehouse/queries/create_customers.sql
@@ -0,0 +1,7 @@
+INSERT INTO Customer (customer_name, customer_phone, customer_email, is_member) VALUES
+    ('Nicholas Rios', '111-111-1111', 'nicholas.rios@rutgers.edu', 1),
+    ('John Doe', '777-777-7777', 'john.doe@rutgers.edu', 1),
+    ('Jane Doe', '888-888-8888', 'jane.doe@rutgers.edu', 0),
+    ('Emily Johnson', '222-222-2222', 'emily.johnson@rutgers.edu', 0),
+    ('Michael Smith', '333-333-3333', 'michael.smith@rutgers.edu', 1),
+    ('Sophia Rodriguez', '444-444-4444', 'sophia.rodriguez@rutgers.edu', 0),
